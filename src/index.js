3b462ce140a60f7c1915f37f302c3a1539748e31
Daniel Rooker <dannyrooks@gmail.com>
2020-03-02 18:37:17 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    newImage();
+    addBreed();
+});
+
+function newImage() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(result => {
+            result.message.forEach(image => addImage(image))
+        });
+}
